a683f0ca26417b9f2fc49af53faed8ec2f8529de
fkuperma <fkuperma@student.touro.edu>
2023-02-09 19:50:46 -0500

    second commit

---
+++ src/component/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Todo = () => {
+    return(
+        <div>
+        <h1>Todos</h1>
+        <p>Cook for Shabbos</p>
+        <p>College Work</p>
+        <p>Hang out with Friends</p>
+        </div>
+    );
+};
